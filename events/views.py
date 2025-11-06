b1757544bf77231a4a7e38221b49ccbfa2647f0b
Mars <111339709+MarsGars9@users.noreply.github.com>
2025-11-06 01:38:24 -0600

    past_events is up | html beginning phase

--- events/views.py
+++ events/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from events.models import Event
 
-# Create your views here.
+
+def past_events(request):
+    past_events = Event.objects.order_by('-date')[:5]
+    
+    context = {
+        'past_events': past_events
+    }
+
+    return render(request, 'events/past_events.html', context)
